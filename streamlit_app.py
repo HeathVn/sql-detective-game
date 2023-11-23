0291de98e443d2788a5b14ffa1e2ab50c22bbc52
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 11:25:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,8 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import subprocess
+import matplotlib.pyplot as plt
 
-
-packages = ["matplotlib"]
-install_command = ["pip", "install"] + packages
-subprocess.call(install_command)
-
-import matplotlib as mt
-
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 # Function to update the text position in the animation
 def update(frame):
@@ -39,8 +22,4 @@ text = ax.text(x0, y0, 'Hello! welcome to Murder Mystery Detectives!', fontsize=
 ax.set_xlim(-1, 5)
 ax.set_ylim(-1, 1)
 
-# Create the animation
-ani = animation.FuncAnimation(fig, update, frames=range(100), interval=50, blit=True)
-
-# Show the animation
-plt.show()
+st.pyplot(fig)
