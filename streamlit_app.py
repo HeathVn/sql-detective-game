aa8924caae165d868ab281542901234ec699d302
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 17:56:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import time
 import hydralit as hy
-
+from streamlit_lottie import st_lottie
 
 def typewriter(text: str, speed: int):
     tokens = text.split()
@@ -13,13 +13,38 @@ def typewriter(text: str, speed: int):
         container.markdown(curr_full_text, unsafe_allow_html=True)
         time.sleep(1/speed)
 
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
 
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+    
+
+lottie_coding = load_lottiefile("lottiefile.json")  # replace link to local lottie file
+lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_M9p23l.json")
 
 app = hy.HydraApp(title='Simple Multi-Page App')
 
 @app.addapp(is_home=True)
 def my_home():
     typewriter(text='<h1>Hello! Welcome to Murder Mystery Detectives!</h1>', speed=3)
+
+    st_lottie(
+    lottie_hello,
+    speed=1,
+    reverse=False,
+    loop=True,
+    quality="low", # medium ; high
+    renderer="svg", # canvas
+    height=None,
+    width=None,
+    key=None,
+)
     hy.info('')
 
 @app.addapp(title='Start Game',icon="🎮")
