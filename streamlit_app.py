43e25bf49b8919dd4836296a0224d8b839b9037a
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 16:21:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,6 @@ import time as time
 import hydralit as hy
 
 
-custom_css = """
-<style>
-    body {
-        background-color: #f0f0f0; /* Light gray background color */
-    }
-
-    .my-text {
-        color: blue;
-        font-size: 18px;
-    }
-
-    .navbar {
-            overflow: hidden;
-            background-color: lightblue;
-            width:100vw;
-            height: 80px;
-            margin:0;
-            
-    }
-</style>
-"""
 
 # Inject custom CSS
 st.markdown(custom_css, unsafe_allow_html=True)
