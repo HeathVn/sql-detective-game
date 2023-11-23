8feb0aff2358b58dafd3b7a06af878bb66cad7b7
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 16:06:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,57 +33,6 @@ def app3():
 #Run the whole lot, we get navbar, state management and app isolation, all with this tiny amount of work.
 app.run()
 
-'''
-#
-components.html(
-"""
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
 
-        .navbar {
-            overflow: hidden;
-            background-color: lightblue;
-            width:100vw;
-            height: 80px;
-            margin:0;
-        }
-
-        .navbar a {
-            
-            display: inline;
-            color: white;
-            text-align: center;
-            padding: 24px 24px;
-            position:relative;
-            text-decoration: none;
-        }
-
-        .navbar a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-        
-        .streamlit-container {
-            max-width: 100%;
-        }
-    </style>
-</head>
-<body>
-
-    <div class="navbar">
-        <a href="#home">Home</a>
-        <a href="#page1">Page 1</a>
-        <a href="#page2">Page 2</a>
-    </div>
-</body>
-
-""")
-'''
 
 st.write("Hello! Welcome to Murder Mystery Detectives! ")
