8afaf7ef7267e91286fd44b37f2da8ef18f19710
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 14:09:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,26 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+import hydralit as hy
+
+app = hy.HydraApp(title='Simple Multi-Page App')
+
+@app.addapp(is_home=True)
+def my_home():
+ hy.info('Hello from Home!')
+
+@app.addapp()
+def app2():
+ hy.info('Hello from app 2')
+
+@app.addapp(title='The Best', icon="🥰")
+def app3():
+ hy.info('Hello from app 3, A.K.A, The Best 🥰')
+
+#Run the whole lot, we get navbar, state management and app isolation, all with this tiny amount of work.
+app.run()
+
+
 components.html(
 """
 <head>
