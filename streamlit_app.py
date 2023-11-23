e06733a8fe010d5f324b43cebde2ad64d2789de6
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 13:33:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,47 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-full_width_style = """
-<style>
-    .navbar {
-        background-color: #FFFFFF;
-        width: 100%;
-        text-align: center;
-        box-sizing: border-box;
-    }
+components.html(
+"""
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
 
-    .navbar a{
-            background-color: #ddd;
+        .navbar {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .navbar a {
             float: left;
             display: block;
             color: white;
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
-     }
+        }
 
-     .block-container{
-        width: 100%;
-     }
-</style>
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="navbar">
+        <a href="#home">Home</a>
+        <a href="#page1">Page 1</a>
+        <a href="#page2">Page 2</a>
+    </div>
+</body>
+
 """
-
-# Apply the custom CSS
-st.markdown(full_width_style, unsafe_allow_html=True)
-
-st.markdown("<div class='navbar'><a href='#home'>Home</a><a href='#page1'>Page 1</a><a href='#page2'>Page 2</a></div>", unsafe_allow_html=True)
+)
 
 st.write("Hello! Welcome to Murder Mystery Detectives! ")
