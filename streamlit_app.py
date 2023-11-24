3dc8dfdbfb9b2cb724d13d9b5b005dcc600ed306
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-23 21:31:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def load_lottieurl(url: str):
 lottie_coding = load_lottiefile("detective.json")  # replace link to local lottie file
 lottie_hello = load_lottieurl("https://lottie.host/7867624f-734c-48fd-8407-94a8f54fbb63/cEi3XEcPWT.json")
 
-
+'''
 st_lottie(
     lottie_hello,
     speed=1,
@@ -63,6 +63,7 @@ st_lottie(
     width=None,
     key=None,
 )
+'''
 
 app = hy.HydraApp(title='Simple Multi-Page App')
 
