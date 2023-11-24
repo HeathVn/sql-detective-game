1842de73d862c4b12b5be83be717a96a3619dc5c
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-24 10:24:55 -0500

    #fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ st.markdown(f"""<div style='{container_style}'>{st_lottie(lottie_coding,key="lot
 typewriter(text=["<h2 style='text-align:center;'>Hello! Welcome to Murder Mystery Detectives!</h1>","<h2 style='text-align:center;'>Your journey begins here</h1>","<h2 style='text-align:center;'>Press the button below to start your game</h1>"], speed=2.5)
 
 
-col1, col2, col3,col4 = st.columns(4)
+col1, col2, col3,col4,col5 = st.columns(5)
 
 with col1:
     pass
@@ -81,7 +81,9 @@ with col3 :
     center_button = st.button(label="Start Game",key="button1")
 with col4 :
     pass
-
+with col5 :
+    pass
+    
 # Use st.markdown to display the HTML content
 #st.markdown(f"""<div style='{button_style}'>{st.button(label="Start Game",key="button1")}</div>""", unsafe_allow_html=True)
  
