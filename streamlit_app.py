fab10b616b27bc898e860fe17bba165505560f48
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-24 00:04:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ from streamlit.components.v1 import html
 lottie_url = "https://lottie.host/embed/2e9086c6-9faf-451a-9880-af787469a8b1/F9hsq1ytrf.json"
 
 container_style = """
-    width:400px;
+    width:100px;
+    height:100px;
     position:relative;
     margin-left:auto;
     margin-right:auto;
