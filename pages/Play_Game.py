c936fe0ef7ef8a20c3099d463e7f92bb73882586
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-25 17:45:31 -0500

    test

--- pages/Play_Game.py
+++ pages/Play_Game.py
@@ -17,7 +17,9 @@ player_name = st.text_input('Player Name:')
 split_data = player_name.split(',')
 
 st.write(split_data[0])
-#data = [ split_data[0], split_data[1] ]
+data = [ split_data[0], split_data[1] ]
+
+st.write(data)
 
 #df = pd.DataFrame(data, columns=['Name1'])
 
