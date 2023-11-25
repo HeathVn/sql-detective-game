62ac790aa6373192fca39e2aac7d4e87d84a663c
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-25 17:51:10 -0500

    test

--- pages/Play_Game.py
+++ pages/Play_Game.py
@@ -18,10 +18,13 @@ split_data = player_name.split(',')
 
 st.write(split_data[0])
 
-data = [ split_data[0], split_data[1] ]
+name1 = split_data[0]
+name2 = split_data[1]
+
+data = [ name1, name2 ]
 
 st.write(data)
 
-df = pd.DataFrame(data, columns=['Name1'])
+df = pd.DataFrame( data , columns=['Name1'])
 
 #st.dataframe(df)
