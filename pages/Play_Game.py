4a5188e081c3c68758a485c3bb2afe9e883bb202
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-25 15:26:54 -0500

    Testing pages

---
+++ pages/Play_Game.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import streamlit.components.v1 as components
+import time
+import hydralit as hy
+import json
+import requests
+from streamlit_lottie import st_lottie
+from streamlit.components.v1 import html
+from streamlit_card import card
+
+
+
+player_name = st.text_input('Player Name:')
+
