48d16dd73c66f858b4cf1cad2b49fa93ad1dadc6
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-25 18:46:15 -0500

    test

--- pages/Play_Game.py
+++ pages/Play_Game.py
@@ -17,47 +17,49 @@ cursor = connection.cursor()
 st.write('''Welcome! Please nter your name to begin.''')
 player_name = st.text_input('Player Name:')
 
-st.write('''Let's view the crime scene report we have received for this''')
+if player_name:
+    st.write('''Let's view the crime scene report we have received for this''')
 
-cursor.execute('''
-    SELECT * 
-    FROM crime_scene_report
-    WHERE type = 'murder' 
-    AND city = 'Mellon City' 
-''')
+    cursor.execute('''
+        SELECT * 
+        FROM crime_scene_report
+        WHERE type = 'murder' 
+        AND city = 'Mellon City' 
+    ''')
 
-rows = cursor.fetchall()
+    rows = cursor.fetchall()
 
-column_names = [description[0] for description in cursor.description]
-
-
-if player_name and rows and column_names:
-    df = pd.DataFrame(data=rows, columns=column_names.capitalize())
-    st.table(df)
-
-else:
-    st.warning('No results found.')
+    column_names = [description[0] for description in cursor.description]
 
 
-user_guess = st.text_input(''' Oh no! It looks like someone meddled with the crime scene reports and some of the key information are missing. Solve this secret code below to find out the missing information!''')
+    if rows and column_names:
+        df = pd.DataFrame(data=rows, columns=column_names.capitalize())
+        st.table(df)
+    else:
+        st.warning('No results found.')
 
-if user_guess:
-    user_guess = user_guess.strip()
 
-cursor.execute('''
-    SELECT * 
-    FROM person
-    WHERE LOWER(address_street_name) = ?
-    ORDER BY name DESC
-''', (user_guess.lower(),))  # Using a placeholder and passing the variable as a parameter
+    user_guess = st.text_input(''' Oh no! It looks like someone meddled with the crime scene reports and some of the key information are missing. Solve this secret code below to find out the missing information!''')
 
-rows = cursor.fetchall()
+    if user_guess:
+        user_guess = user_guess.strip()
 
-if rows:
-    df = pd.DataFrame(data=rows, columns=column_names)
-    st.table(df)
-else:
-    print ('No results found. Try again!')
+    cursor.execute('''
+        SELECT * 
+        FROM person
+        WHERE LOWER(address_street_name) = ?
+        ORDER BY name DESC
+    ''', (user_guess.lower(),))  # Using a placeholder and passing the variable as a parameter
+
+    
+    rows = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+
+    if rows:
+        df = pd.DataFrame(data=rows, columns=column_names)
+        st.table(df)
+    else:
+        st.warning('No results found. Try again!')
 
 
 
