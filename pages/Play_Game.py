36b916d64eb10fa60c955d2cf3e9ac18313039d2
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-25 23:43:42 -0500

    test

--- pages/Play_Game.py
+++ pages/Play_Game.py
@@ -50,12 +50,12 @@ lottie_coding = load_lottiefile("riddle1.json")
 
 col1,col2,col3 = st.columns([3,1,3])
 
-        with col1:
-            pass
-        with col2 :
-            st.markdown(f"""<div style="width: 50%; height: 50%;">{st_lottie(lottie_coding,key="lottie1")}</div>""",unsafe_allow_html=True)
-        with col3 :
-            pass  
+with col1 :
+    pass
+with col2 :
+    st.markdown(f"""<div style="width: 50%; height: 50%;">{st_lottie(lottie_coding,key="lottie1")}</div>""",unsafe_allow_html=True)
+with col3 :
+    pass  
 
 st.write('''Welcome! Please enter your name to begin.''')
 player_name = st.text_input('Player Name:')
