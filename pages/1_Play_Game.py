67c9ed5abfe6745403e78e09728e24535456ef3a
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-26 15:49:44 -0500

    test

--- pages/1_Play_Game.py
+++ pages/1_Play_Game.py
@@ -58,17 +58,7 @@ def typewriter(text: [str,str], speed: int):
             container.markdown(curr_full_text, unsafe_allow_html=True)
             time.sleep(1/speed)
 
-def typewriter2(text: str, speed: int):
-    
-    container = st.empty()
-   
-    tokens = text.split() 
 
-    for index in range(len(tokens) + 1):
-        curr_full_text = " ".join(tokens[:index])
-        container.markdown(curr_full_text, unsafe_allow_html=True)
-        time.sleep(1/speed)
-    
 
 lottie_coding = load_lottiefile("riddle1.json")
 
@@ -144,7 +134,7 @@ if player_name:
         st.image('detective-profile.png')
     with col2 :
        st.write(f'''Welcome aboard, Detective {player_name}!''')
-       typewriter(['''We find ourselves at a critical juncture in Mellon City—a murder on January 15, 2018. The entire city is counting on your super-sleuth skills to crack the case. Before we dive into the nitty-gritty, let's snag the lowdown on the crime scene. Head on over to the police department's database and grab those crime scene reports. The city's counting on you! Good luck!'''],3)
+       st.write('''We find ourselves at a critical juncture in Mellon City—a murder on January 15, 2018. The entire city is counting on your super-sleuth skills to crack the case. Before we dive into the nitty-gritty, let's snag the lowdown on the crime scene. Head on over to the police department's database and grab those crime scene reports. The city's counting on you! Good luck!'''])
 
     cursor.execute('''
         SELECT * 
@@ -167,7 +157,7 @@ if player_name:
         with col1 :
             st.image('detective-profile.png')
         with col2 :
-            typewriter([f'''Well, Detective {player_name}, you've absorbed the details. When you're ready to plunge into the investigation, hit that button and let's unravel this mystery together! '''], 3)
+            st.write(f'''Well, Detective {player_name}, you've absorbed the details. When you're ready to plunge into the investigation, hit that button and let's unravel this mystery together! ''')
         
         col1,col2 = st.columns([6,1])
 
