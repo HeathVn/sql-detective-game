62bf5d5928b6806c53ea24f7887027eb596c3ad3
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-26 23:36:37 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,13 +128,13 @@ with st.sidebar:
     st.text('Total Players:')
 
 st.divider()
-st.markdown('### Get Started')
+
 col1, col2, col3 = st.columns([1.8,2,1])
 
 with col1:
     pass
 with col2:
-    st.markdown(f'''<div style='margin-left:auto;margin-right:auto; position:relative;'> {st.markdown('### Get Started')} </div>''', unsafe_allow_html=True)
+    st.markdown(f"""<div style='margin-left:auto;margin-right:auto; position:relative;'> {st.markdown('### Get Started')} </div>""", unsafe_allow_html=True)
     
 with col3 :
     pass
