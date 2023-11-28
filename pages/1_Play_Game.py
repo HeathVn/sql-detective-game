752eb1f244becc4d052168ae9fabc29f8a74563e
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-27 20:44:10 -0500

    test

--- pages/1_Play_Game.py
+++ pages/1_Play_Game.py
@@ -212,7 +212,7 @@ if player_name:
         with col2 :
             
             st.markdown(f"""<div style='width:20px;'>{st_lottie(lottie_coding,key="lottie1")}</div>""",unsafe_allow_html=True)
-                
+            time.sleep(1)    
             
         with col3:
             pass
