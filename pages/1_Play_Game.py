3c612a887727f797ed040964d5b8bbcc889c4333
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-27 20:55:40 -0500

    test

--- pages/1_Play_Game.py
+++ pages/1_Play_Game.py
@@ -213,7 +213,7 @@ if player_name:
             
             with st.empty():
                 st_lottie(lottie_coding,key="lottie1")
-                st.markdown(f"<div style=''></div>") 
+                st.markdown("<div style=''></div>") 
             
         with col3:
             pass
