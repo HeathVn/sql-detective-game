897f4843e387d676ced07fc8289fcbb04924dff4
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-11-27 20:34:13 -0500

    testing

--- pages/1_Play_Game.py
+++ pages/1_Play_Game.py
@@ -213,8 +213,8 @@ if player_name:
             container4 = st.empty()
             with container4:
                 for i in range(0,10):
-                    container4.markdown(f"""<div style='width:20px;'>{st_lottie(lottie_coding,key="lottie1")}</div>""",unsafe_allow_html=True)
-                    
+                    st.markdown(f"""<div style='width:20px;'>{st_lottie(lottie_coding,key="lottie1")}</div>""",unsafe_allow_html=True)
+                    time.sleep(1)
         with col3:
             pass
 
