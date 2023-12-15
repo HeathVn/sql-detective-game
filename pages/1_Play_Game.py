0d4107d5242dc77314812c446c9f0abc6552979d
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-15 16:12:10 -0500

    murderer update

--- pages/1_Play_Game.py
+++ pages/1_Play_Game.py
@@ -433,7 +433,7 @@ if player_name:
                                             typewriter([f'''Incredible detective work! {user_murderer.capitalize()} is indeed the murderer. Mellon City extends its deepest gratitude; this case wouldn't have been cracked without your skillful unraveling.'''],3)
                                             
                                         cursor.execute('''
-                                            SELECT p.name
+                                            SELECT *
                                             FROM drivers_license dl
                                             INNER JOIN person p 
                                             ON dl.id = p.license_id 
