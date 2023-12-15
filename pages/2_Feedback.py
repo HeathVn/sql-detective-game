4d119fc70d3fdfa1c01987dc4d60343e3e730204
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-15 17:51:18 -0500

    test star ratings

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -6,7 +6,7 @@ with st.form("user_feedback"):
     header[1].subheader('Feedback Form')
 
     stars = st_star_rating(label, amount_of_stars, default_value, size, emoticons, read_only, dark_theme, resetButton=reset_btn, resetLabel=reset_label,
-                       #customCSS=css_custom, on_click=function_to_run_on_click if enable_on_click else None)
+                       customCSS=css_custom, on_click=function_to_run_on_click if enable_on_click else None)
 
 
 
