26c681e7fae5da6905f2ae16314a5aeced2842a2
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-15 18:15:25 -0500

    adjust star rating size

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -5,7 +5,7 @@ with st.form("user_feedback"):
     header = st.columns([0.8,1,0.5])
     header[1].subheader('Feedback Form')
 
-    stars = st_star_rating(label = "Please rate you experience", maxValue = 5, defaultValue = 3, key = "rating", dark_theme = True , size=12)
+    stars = st_star_rating(label = "Please rate you experience", maxValue = 5, defaultValue = 3, key = "rating", dark_theme = True , size=15)
 
 
 
