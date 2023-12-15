b4857f6852ce9d9693e0ef233aa4b12590a95ab3
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-15 17:50:51 -0500

    testing star ratings

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -5,7 +5,7 @@ with st.form("user_feedback"):
     header = st.columns([0.8,1,0.5])
     header[1].subheader('Feedback Form')
 
-   # stars = st_star_rating(label, amount_of_stars, default_value, size, emoticons, read_only, dark_theme, resetButton=reset_btn, resetLabel=reset_label,
+    stars = st_star_rating(label, amount_of_stars, default_value, size, emoticons, read_only, dark_theme, resetButton=reset_btn, resetLabel=reset_label,
                        #customCSS=css_custom, on_click=function_to_run_on_click if enable_on_click else None)
 
 
