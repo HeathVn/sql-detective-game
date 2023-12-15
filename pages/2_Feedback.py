25fc506cdd99fcdfc9e2acc1b20342df8892c1fa
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-15 18:18:22 -0500

    testing ratings label

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -5,7 +5,8 @@ with st.form("user_feedback"):
     header = st.columns([0.8,1,0.5])
     header[1].subheader('Feedback Form')
 
-    stars = st_star_rating(label = "Please rate you experience", maxValue = 5, defaultValue = 3, key = "rating", dark_theme = True , size=20)
+    label = st.text_input("Label","How many stars?")
+    stars = st_star_rating(label, maxValue = 5, defaultValue = 3, key = "rating", dark_theme = True , size=20)
 
 
 
