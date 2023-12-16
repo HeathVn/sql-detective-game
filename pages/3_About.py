fa159aca894447340508f2dad13d9daa7d66fbe8
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-16 18:35:21 -0500

    added about page

---
+++ pages/3_About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title('About SQL - Mellon City Mysteria')
+    
+st.markdown('SQL - Mellon City Mysteria is an SQL based problem solver game developed by two Carnegie Mellon Graduate students.')
