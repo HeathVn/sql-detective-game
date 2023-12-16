aff490727ae9e17472eb04cd6dd46c8c752d2ea3
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-16 18:21:56 -0500

    testing

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -59,7 +59,7 @@ header = st.columns([0.8,1.5,0.5])
 header[1].title('Feedback Form')
 
 col = st.columns([0, 18 ,0])
-col[1].text('''Welcome to our Feedback Form! 
+st.text('''Welcome to our Feedback Form! 
 
 Thank you for being a part of our programming adventure. Your insights are highly 
 valued, and we would be grateful if you could take a moment to share your thoughts 
