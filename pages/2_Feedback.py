6316813f69a35e8c7ea9dc1a67e59320a70554c5
HeathVn <62621793+HeathVn@users.noreply.github.com>
2023-12-16 15:12:17 -0500

    testing

--- pages/2_Feedback.py
+++ pages/2_Feedback.py
@@ -6,10 +6,11 @@ connection = sqlite3.connect('Modified SQL Database.db')
 cursor = connection.cursor()
 
 
+header = st.columns([0.8,1,0.5])
+header[1].title('Feedback Form')
 
 with st.form("user_feedback"):
-    header = st.columns([0.8,1,0.5])
-    header[1].subheader('Feedback Form')
+    
 
     name = st.text_input('''Please enter your name:''')
     stars = st_star_rating(label = "Please rate you experience", maxValue = 5, defaultValue = 3, key = "rating", dark_theme = True , size=20)
@@ -18,9 +19,8 @@ with st.form("user_feedback"):
 
     submit = st.form_submit_button('Submit')
 
-if submit:
-    cursor.execute('''
-        INSERT INTO Players('Player Name', 'Date Played', 'Start Time', 'End Time', 'Rating', 'Any feedback/suugestions')
-        VALUES (?,'','','',?,?) 
-    ''', (name, stars,suggestion))
+    if submit:
+        cursor.execute('''INSERT INTO Players VALUES (?,'','','',?,?)''', (name, stars,suggestion))
+        connection.commit()
+        connection.close()
                                          
